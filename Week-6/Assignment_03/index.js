5af089ee4e00cdb68ffd42779b1af7d5cd31a54a
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-07-23 18:43:35 +0530

    Update index.js

--- Week-6/Assignment_03/index.js
+++ Week-6/Assignment_03/index.js
@@ -3,29 +3,29 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var sortColors = function(nums) {
-         let i = 0; 
-		     let j = 0; 
-		     let k = nums.length - 1; 
-		 
-		 
-		 
-		 while(i <= k) { 
-			 if(nums[i] == 0) {
-			     swap(nums, i, j);
-				 i++;
-				 j++;
-			 } else if(nums[i] == 1) {
-				 i++;
-			 } else {
-				 swap(nums, i, k);
-				 k--;
-			 } 
-		 }
+    let i = 0;
+    let j = 0;
+    let k = nums.length - 1;
+
+
+
+    while (i <= k) {
+        if (nums[i] == 0) {
+            swap(nums, i, j);
+            i++;
+            j++;
+        } else if (nums[i] == 1) {
+            i++;
+        } else {
+            swap(nums, i, k);
+            k--;
+        }
+    }
 };
 
 
 function swap(nums, i, j) {
-		    let temp = nums[i];
-		    nums[i] = nums[j];
-		    nums[j] = temp;
-		  }
+    let temp = nums[i];
+    nums[i] = nums[j];
+    nums[j] = temp;
+}
