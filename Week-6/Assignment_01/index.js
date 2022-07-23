f336cf6d2efbf3fd8d7f7f235cec31b3dc6c0049
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-07-23 22:03:01 +0530

    Update index.js

--- Week-6/Assignment_01/index.js
+++ Week-6/Assignment_01/index.js
@@ -1,1 +1,30 @@
-
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+    if(nums.length === 0) {
+        return 0;
+    }
+        
+    if(nums.length === 1) {
+        return nums[0];
+    }
+    
+    let maxSum = -Infinity;
+    let curSum = 0;
+    
+    for(let i = 0; i < nums.length; i++) {
+        curSum = curSum + nums[i];
+        
+        if (curSum > maxSum) {
+            maxSum = curSum;
+        }
+        
+        if(curSum < 0) {
+            curSum = 0;
+        }
+    }
+    
+    return maxSum;
+};
