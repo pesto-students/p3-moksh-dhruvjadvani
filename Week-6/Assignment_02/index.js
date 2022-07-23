017d0e93b9dda58d48c55c9d82c58f2a8b5d8b9f
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-07-23 21:45:17 +0530

    Week 6 Assignment 2

--- Week-6/Assignment_02/index.js
+++ Week-6/Assignment_02/index.js
@@ -3,46 +3,50 @@
  * @return {number[][]}
  */
 var generateMatrix = function(n) {
+    
+    let output = new Array(n).fill(0).map(() => new Array(n).fill(0))
+    
+    let count = 0;
+    let size = n * n;
+    
 
-  let output = new Array(n).fill(0).map(() => new Array(n).fill(0))
-  let count = 0;
-  let size = n * n;
-  let left = 0;
-  let right = n - 1;
-  let top = 0;
-  let bottom = n - 1;
-
-  while (count < size) {
-
-    //going left
-    for (let i = left; i <= right; i++) {
-      count++;
-      output[top][i] = count;
+    let minCol = 0; // left
+    let maxCol = n - 1; // right
+    let minRow = 0; // top
+    let maxRow = n - 1; //bottom
+    
+    while(count < size){
+        
+        //going left
+        for(let i = minCol; i <= maxCol; i++){
+            count++;
+            output[minRow][i] = count;
+        }
+        minRow++;
+                
+        // going down
+        for(let i = minRow; i <= maxRow; i++){
+            count++;
+            output[i][maxCol] = count;
+        }
+        maxCol--;
+        
+        //going left
+        for(let i = maxCol; i >= minCol; i--){
+            count++;
+            output[maxRow][i] = count;
+        }
+        maxRow--;
+        
+        //going up
+        for(let i = maxRow; i >= minRow; i--){
+            count++;
+            output[i][minCol] = count;
+        }
+        minCol++;
     }
-    top++;
-
-    // going down
-    for (let i = top; i <= bottom; i++) {
-      count++;
-      output[i][right] = count;
-    }
-    right--;
-
-    //going left
-    for (let i = right; i >= left; i--) {
-      count++;
-      output[bottom][i] = count;
-    }
-    bottom--;
-
-    //going up
-    for (let i = bottom; i >= top; i--) {
-      count++;
-      output[i][left] = count;
-    }
-    left++;
-  }
-
-  return output;
-
+    
+    return output;
+    
 };
+    
