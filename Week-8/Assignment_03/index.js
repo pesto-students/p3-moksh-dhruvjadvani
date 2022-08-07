8a6b1c409a49aea83fab864064508127f9d1743e
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-08-08 02:29:41 +0530

    Update index.js

--- Week-8/Assignment_03/index.js
+++ Week-8/Assignment_03/index.js
@@ -1,1 +1,34 @@
-
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrder = function(root) {
+    let result = [];
+    currentLevelNodes = [];
+    if(root) 
+        currentLevelNodes.push(root);
+    while(currentLevelNodes.length > 0) {
+        current = [];
+        let len = currentLevelNodes.length;
+        for (let i = 0; i< len; i++) {
+            let node = currentLevelNodes.shift();
+            current.push(node.val);
+            if(node.left) {
+                currentLevelNodes.push(node.left);
+            }
+            if(node.right) {
+                currentLevelNodes.push(node.right);
+            }
+        }
+        result.push(current);
+    }
+    return result;
+};
