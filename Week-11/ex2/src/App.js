18cf69780552fec599868822dadb7f15c5af6bfe
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-09-28 11:51:08 +0530

    Update App.js

--- Week-11/ex2/src/App.js
+++ Week-11/ex2/src/App.js
@@ -1,1 +1,18 @@
+import { useState } from 'react';
+import { Button } from 'react-bootstrap';
+import { connect } from 'react-redux';
+import { mapDispatchToProps, mapStateToProps } from './store/counterDispatch';
 
+function App(props) {
+    console.log(props);
+    const { counter, increment, reset } = props;
+    return (
+        <div className='App'>
+            <h1>{counter}</h1>
+            <Button onClick={increment}>Increment</Button>
+            <Button onClick={reset}>Reset</Button>
+        </div>
+    );
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
